7bb4bb63a425b07fd57bb2a5a5415a7d2d2ef2ca
innovate <quantainnovate@gmail.com>
2020-11-07 20:36:43 +0700

    default project

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const Path = require('path');
+
+
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    await server.register([{
+        plugin: require('hapi-auto-route'),
+        options: {
+            routes_dir: Path.join(__dirname, 'routes')
+        }
+    }]);
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+
+init();
---
+++ routes/routes.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Hello World!';
+        }
+    }
+];
